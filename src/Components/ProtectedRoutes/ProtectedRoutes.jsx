7358734aa4cb72bf8550bf843b98fda1a70ef075
Ahmed Mosaad <ahmedmosad840@gmail.com>
2025-09-16 17:13:26 +0300

    ProtectedRoutes edit

--- src/Components/ProtectedRoutes/ProtectedRoutes.jsx
+++ src/Components/ProtectedRoutes/ProtectedRoutes.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Navigate } from "react-router-dom";
+import { AuthContext } from "../../Context/AuthContext";
 
-export default function ProtectedRoutes({children}) {
-  if (localStorage.getItem("userToken")) {
+export default function ProtectedRoutes({ children }) {
+  const { userToken } = useContext(AuthContext);
+  if (userToken) {
     return children;
   } else {
     return <Navigate to={"/login"} />;
